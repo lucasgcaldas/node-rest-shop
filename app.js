3cdec9016e96d9fe1d82516a9502be3a3222379c
Lucas Gomes <lggomes007@gmail.com>
2022-03-02 15:04:46 -0300

    Parsing the Body & Handling CORS

--- app.js
+++ app.js
@@ -1,16 +1,32 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const bodyParser = require('body-parser')
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
-const req = require('express/lib/request');
 
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Acess-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
+});
 
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
 
+// Routes which should handle request
 app.use((req, res, next) => {
     const error = new Error('Not found');    
     error.status = 404;
