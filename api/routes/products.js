4aa0a556f7f9c88b6ef57a5bb681a944235975c3
Lucas Gomes <lggomes007@gmail.com>
2022-03-07 14:47:27 -0300

    Managing Orders with Mongoose

--- api/routes/products.js
+++ api/routes/products.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 
-const Product = require('../models/products')
+const Product = require('../models/product')
 
 router.get("/", (req, res, next) => {
     Product.find()
@@ -22,7 +22,7 @@ router.get("/", (req, res, next) => {
                         }
                     }
                 })
-            }   
+            }
 
             res.status(200).json(response);
         })
@@ -57,7 +57,7 @@ router.post("/", (req, res, next) => {
     })
         .catch(err => {
             console.log(err);
-            res.status(500).json({err: err});
+            res.status(500).json({ err: err });
         });
 });
 
@@ -68,7 +68,7 @@ router.get("/:productId", (req, res, next) => {
         .exec()
         .then(doc => {
             console.log("From database", doc);
-            if(doc){
+            if (doc) {
                 res.status(200).json({
                     product: doc,
                     request: {
@@ -77,13 +77,13 @@ router.get("/:productId", (req, res, next) => {
                     }
                 });
             } else {
-                res.status(404).json({message: 'No valid entry found for providaded ID'});
+                res.status(404).json({ message: 'No valid entry found for providaded ID' });
             }
             res.status(200).json(doc);
         })
         .catch(err => {
             console.log(err);
-            res.status(500).json({err: err});
+            res.status(500).json({ err: err });
         });
 });
 
@@ -93,7 +93,7 @@ router.patch("/:productId", (req, res, next) => {
     for (const ops of req.body) {
         updateOps[ops.propName] = ops.value
     }
-    Product.updateOne({_id: id}, { $set: updateOps })
+    Product.updateOne({ _id: id }, { $set: updateOps })
         .exec()
         .then(result => {
             console.log(result);
@@ -103,17 +103,17 @@ router.patch("/:productId", (req, res, next) => {
                     type: 'GET',
                     url: "http://localhost:3000/products/" + id
                 }
-            });        
+            });
         })
         .catch(err => {
             console.log(err);
-            res.status(500).json({err: err});
+            res.status(500).json({ err: err });
         });
 });
 
 router.delete("/:productId", (req, res, next) => {
     const id = req.params.productId;
-    Product.remove({_id: id})
+    Product.remove({ _id: id })
         .exec()
         .then(result => {
             res.status(200).json({
@@ -121,13 +121,13 @@ router.delete("/:productId", (req, res, next) => {
                 request: {
                     type: 'POST',
                     url: "http://localhost:3000/products/",
-                    body: { name: 'String', price: 'Number' } 
+                    body: { name: 'String', price: 'Number' }
                 }
-            });        
+            });
         })
         .catch(err => {
             console.log(err);
-            res.status(500).json({err: err});
+            res.status(500).json({ err: err });
         });
 });
 
